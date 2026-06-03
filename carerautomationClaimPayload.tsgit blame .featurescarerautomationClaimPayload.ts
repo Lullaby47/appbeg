8d01acbcdabb30d9bccf1edf2a29fbf2fe85d1bf
Ayush <pokharelayush1@gmail.com>
2026-06-03 19:53:35 +0545

    Fixing automation failed after vps

---
+++ carerautomationClaimPayload.tsgit blame .featurescarerautomationClaimPayload.ts
@@ -0,0 +1,10 @@
+[33m6277a20[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Coadmins agent Feature in personal vps
+[33ma699b82[m Coadmins agent Feature in personal vps
+[33med95068[m Coadmins agent Feature in personal vps
+[33mb5e0650[m Coadmins agent Feature in personal vps
+[33m5525610[m Coadmins agent Feature in personal vps
+[33m6ed6c9a[m Coadmins agent Feature in personal vps
+[33mb2ac369[m Players login ui info
+[33mfdcc463[m Firestore reads samsodhan
+[33m0d3e23e[m online sql for username searn coadmins agent
+[33m0e3a166[m GameVault features holding recharge job
